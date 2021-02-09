ac4ac5dee70b7c9ac134c6d6d939d9dd07be25e6
Luke Eltiste <lukeeltiste@csu.fullerton.edu>
2021-02-09 12:36:21 -0800

    Add Description and License

--- assign1/perimiter.asm
+++ assign1/perimiter.asm
@@ -1,14 +1,42 @@
-; Welcome to an friendly assembly program by Luke Eltiste.
-; This program will compute the perimeter and the average side length of a rectangle.
-; Enter the height:   3.75
-; Enter the width:    7.2
-; The perimeter is  21.9
-; The length of the average side is 5.475
-; I hope you enjoyed your rectangle
-; The assembly program will send the perimeter to the main function.
-; The main function received this number 21.9 and has decided to keep it.
-; A 0 will be returned to the operating system.
-; Have a nice day.
+;*****************************************************************************************************************************
+;Program name: "Perimiter of a Rectangle".  This program demonstrates how to input and output float data and how to perform  *
+;form a few simple operations on floats.  Copyright (C) 2021 Luke Eltiste                                                    *
+;This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License   *
+;version 3 as published by the Free Software Foundation.                                                                     *
+;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied          *
+;Warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.      *
+;A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.                            *
+;*****************************************************************************************************************************
+
+;=======1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+;
+;Author information
+;  Author name: Luke Eltiste
+;  Author email: lukeeltiste@csu.fullerton.edu
+;
+;Program information
+;  Program name: Perimiter of a Rectangle
+;  Programming languages: One modules in C++ and one module in X86
+;  Date program began:     2021-Jan-28
+;  Date program completed: 2021-Feb-09
+;  Date comments upgraded: 2021-Feb-09
+;  Files in this program: rectangle.cpp, perimiter.asm, run.sh
+;  Status: Complete.  No errors found after extensive testing.
+;
+;References for this program
+;  Jorgensen, X86-64 Assembly Language Programming with Ubuntu, Version 1.1.40.
+;  Robert Plantz, X86 Assembly Programming.  [No longer available as a free download]
+;
+;Purpose
+;  Show how to perform arithmetic operations on float data types
+;
+;This file
+;   File name: perimiter.asm
+;   Language: X86-64 with Intel syntax
+;   Max page width: 132 columns
+;   Assemble: nasm -f elf64 -l perimiter.lis -o perimiter.o perimiter.asm
+;   Link: gcc -m64 -no-pie -o perimiterOfRectangle.out -std=c++17 perimiter.o rectangle.o
+;   Optimal print specification: 132 columns width, 7 points, monospace, 8½x11 paper
 
 extern printf
 extern scanf
@@ -25,7 +53,7 @@ floatformat db "%lf", null                                                      
 stringformat db "%s", null                                                                                      ; Message Format
 enterheight db "Enter the height:   ", null                                                                     ; Ask for height
 enterwidth db "Enter the width:    ", null                                                                     ; Ask for width
-perimeterprint db "The perimeter is %3.6lf", 10, 0                                                              ; Print the perimeter
+perimeterprint db "The perimeter is %3.6lf", newline, null                                                              ; Print the perimeter
 avglength db "The length of the average side is %3.6lf", newline, null                                          ; Print the average side
 enjoyrec db "I hope you enjoyed your rectangle", newline, null                                                  ; Goodbye Message
 return db "The assembly program will send the perimeter to the main function.", newline, null                   ; Return Message
--- assign1/rectangle.cpp
+++ assign1/rectangle.cpp
@@ -1,29 +1,62 @@
-/*
-Welcome to an friendly assembly program by Luke Eltiste.
-This program will compute the perimeter and the average side length of a rectangle.
-Enter the height:   3.75
-Enter the width:    7.2
-The perimeter is  21.9
-The length of the average side is 5.475
-I hope you enjoyed your rectangle
-The assembly program will send the perimeter to the main function.
-The main function received this number 21.9 and has decided to keep it.
-A 0 will be returned to the operating system.
-Have a nice day.
-*/
+//*****************************************************************************************************************************
+//Program name: "Perimiter of a Rectangle".  This program demonstrates how to input and output float data and how to perform  *
+//form a few simple operations on floats.  Copyright (C) 2021 Luke Eltiste                                                    *
+//This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License   *
+//version 3 as published by the Free Software Foundation.                                                                     *
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied          *
+//Warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.      *
+//A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.                            *
+//*****************************************************************************************************************************
+
+
+//=======1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+//
+//Author information
+//  Author name: Luke Eltiste
+//  Author email: lukeeltiste@csu.fullerton.edu
+//
+//Program information
+//  Program name: Perimiter of a Rectangle
+//  Programming languages: One modules in C++ and one module in X86
+//  Date program began:     2021-Jan-28
+//  Date program completed: 2021-Feb-09
+//  Date comments upgraded: 2021-Feb-09
+//  Files in this program: rectangle.cpp, perimiter.asm, run.sh
+//  Status: Complete.  No errors found after extensive testing.
+//
+//References for this program
+//  Jorgensen, X86-64 Assembly Language Programming with Ubuntu
+//
+//Purpose
+//  Show how to perform arithmetic operations on float data types
+//
+//This file
+//   File name: rectangle.cpp
+//   Language: C++
+//   Max page width: 132 columns
+//   Compile: gcc -c -Wall -m64 -no-pie -o rectangle.o rectangle.cpp -std=c++17
+//   Link: gcc -m64 -no-pie -o perimiterOfRectangle.out -std=c++17 perimiter.o rectangle.o
+//   Optimal print specification: 132 columns width, 7 points, monospace, 8½x11 paper
+//
+//
+//=======1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
+//
+//
+//
+//
+//===== Begin code area ===========================================================================================================
 
 #include <stdio.h>
-#include <stdint.h>
 
 extern "C" double perimeter();
 
 int main()
 {
-    double result_code = -999;
-    printf("%s\n", "Welcome to an friendly assembly program by Luke Eltiste.");
-    result_code = perimeter();
-    printf("%s%lf%s\n","The main function received this number ",result_code," and has decided to keep it.");
-    printf("%s\n", "A 0 will be returned to the operating system.");
-    printf("%s\n", "Have a nice day.");
-    return 0;
-} //End of main
+  double result_code = -999;
+  printf( "Welcome to an friendly assembly program by Luke Eltiste.\n" );
+  result_code = perimeter();
+  printf( "The main function received this number %lf and has decided to keep it.\n", result_code );
+  printf( "A 0 will be returned to the operating system.\n" );
+  printf( "Have a nice day.\n" );
+  return 0;
+}    //End of main
